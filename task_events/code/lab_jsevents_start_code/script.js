a4f196db1260ee08b76e0c010291a4c491553349
Joseph Vincenzo Carpentieri <joecarpentieri1@gmail.com>
2023-11-27 15:32:57 +0000

    can add to list

--- task_events/code/lab_jsevents_start_code/script.js
+++ task_events/code/lab_jsevents_start_code/script.js
@@ -1,6 +1,28 @@
 const toDoForm = document.querySelector("#todo-form");
 const enterButton = document.querySelector("#enter");
+const textInput = document.querySelector("#text-input");
+const deleteButton = document.querySelector("#delete");
+const showDateButton = document.querySelector("#show-date");
+const toDoList = document.querySelector("#list");
 
-enterButton.addEventListener("click", () => {
+toDoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
     console.log("button clicked");
+    // get the input
+    const createdListItem = document.createElement("li");
+    createdListItem.innerText = event.target["new-todo"].value;
+    list.appendChild(createdListItem);
 })
+
+// textInput.addEventListener("input", (event) => {
+//     console.log(event);
+// })
+
+// deleteButton.addEventListener("click", () => {
+//     console.log("deleted");
+// })
+
+toDoForm.addEventListener("submit", (event) => {
+    toDoForm.textInput = event.target.value;
+})
+
