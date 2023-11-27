3658d6a0d9116fde9cee688525b481f0be3c8731
Suzi Towers <suzitowers@gmail.com>
2023-11-27 14:18:34 +0000

    added enterButton event listener

---
+++ task_events/code/lab_jsevents_start_code/script.js
@@ -0,0 +1,6 @@
+const toDoForm = document.querySelector("#todo-form");
+const enterButton = document.querySelector("#enter");
+
+enterButton.addEventListener("click", () => {
+    console.log("button clicked");
+})
